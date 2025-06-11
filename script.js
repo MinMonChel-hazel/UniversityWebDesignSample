65b413035b36092ff25c321e845d97b86423cb13
Min Mon Chel <minmonchel@gmail.com>
2025-06-11 17:17:16 +0700

    Already Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// let navLinks = document.getElementById("nav-links");
+
+// const showMenu = () => {
+//     navLinks.style.width = "200px";
+// }
+
+// const hideMenu = () => {
+//     navLinks.style.width = "0px";
+// }
+
+const navLinks = document.getElementById("nav-links")
+
+const showMenu = () => {
+    navLinks.style.width = "150px";
+}
+
+const hideMenu = () => {
+    navLinks.style.width = "0px";
+    
+}
